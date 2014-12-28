4eac388504f7d8d8e1b1b710465f0ed0595513a2
Spencer Krum <nibz@cat.pdx.edu>
2014-12-27 17:09:04 -0800

    pep8 for snot rocket tests

--- snotrocket/test_snotrocket_sync.py
+++ snotrocket/test_snotrocket_sync.py
@@ -9,23 +9,11 @@ import time
 
 import requests
 
-"""
->>> r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
->>> r.status_code
-200
->>> r.headers['content-type']
-'application/json; charset=utf8'
->>> r.encoding
-'utf-8'
->>> r.text
-u'{"type":"User"...'
->>> r.json()
-{u'private_gists': 419, u'total_private_repos': 77, ...}
-"""
 
 def validate_assigned(ticket_number, assigned_to):
     time.sleep(1)
-    r = requests.get('http://localhost:9200/snotrocket/snot-ticket/{0}'.format(ticket_number))
+    url = 'http://localhost:9200/snotrocket/snot-ticket/'
+    r = requests.get(url + ticket_number)
     actually_assigned_to = r.json()['_source'].get('assigned_to')
     #print "debug"
     #print actually_assigned_to, assigned_to
