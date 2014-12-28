10ea56f4e92b7ed3233219381918d79547da6212
Spencer Krum <nibz@cat.pdx.edu>
2014-12-27 16:11:24 -0800

    boogerd: adding metadata and flags endpoints

--- boogerd2/boogerd2.py
+++ boogerd2/boogerd2.py
@@ -8,6 +8,7 @@ import pysnot
 from flask import Flask, abort, request, jsonify
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello World!"
@@ -19,11 +20,27 @@ def ticket_get(ticket_number):
     tic = pysnot.get_ticket(ticket_number)
     return tic
 
+
 @app.route("/v1/ticket/<ticket_number>/flags")
 def ticket_flags_get(ticket_number):
 
     flags = pysnot.get_flags(ticket_number)
     return flags
+
+
+@app.route("/v1/ticket/<ticket_number>/assigned")
+def ticket_assigned_get(ticket_number):
+
+    assigned = pysnot.get_assigned(ticket_number)
+    return assigned
+
+
+@app.route("/v1/ticket/<ticket_number>/metadata")
+def ticket_metadata_get(ticket_number):
+
+    metadata = pysnot.get_metadata(ticket_number)
+    return jsonify(metadata)
+
 
 @app.route("/v1/all_flags")
 def all_flags():
@@ -31,6 +48,7 @@ def all_flags():
     flags = pysnot.list_all_flags()
     #this looks funky because of http://flask.pocoo.org/docs/0.10/security/#json-security
     return jsonify({"all_flags": flags})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- boogerd2/boogerdtest.sh
+++ boogerd2/boogerdtest.sh
@@ -8,8 +8,9 @@ failed=false
 boogerd_url='localhost:5000'
 
 if netstat -antlp 2>/dev/null | grep 5000 >/dev/null 2>&1 ; then
-  :
+  echo -n .
 else
+  echo "TEST #0 failed"
   echo "Are you running a boogerd instance?"
   exit 1
 fi
@@ -35,6 +36,39 @@ then
   echo -n .
 else
   echo "TEST #3 failed"
+  exit 1
+fi
+
+if curl -s $boogerd_url/v1/ticket/247/assigned | grep 'nibz' >/dev/null
+then
+  echo -n .
+else
+  echo "TEST #4 failed"
+  exit 1
+fi
+
+output=`curl -s $boogerd_url/v1/ticket/247/metadata`
+if echo $output | grep '"assigned_to": "nibz",' >/dev/null
+then
+  echo -n .
+else
+  echo "TEST #5 failed"
+  exit 1
+fi
+
+if echo $output | grep '"summary_email": "andywood@cat.pdx.edu",' >/dev/null
+then
+  echo -n .
+else
+  echo "TEST #6 failed"
+  exit 1
+fi
+
+if echo $output | grep '"subject": "Your linux session has been terminated.",' >/dev/null
+then
+  echo -n .
+else
+  echo "TEST #7 failed"
   exit 1
 fi
 
