2e3f9c971692d65b2754d9be19ce483f7920c8fd
Spencer Krum <nibz@cat.pdx.edu>
2014-12-31 02:47:05 -0800

    first search function for pysnotrocket

--- snotrocket/pysnotrocket.py
+++ snotrocket/pysnotrocket.py
@@ -24,7 +24,15 @@ def get_ticket_by_number(ticket_number):
     return res['hits']['hits'][0]['_source']
 
 
+def get_open_tickets(index_from=0, size=20):
+    res = es.search(index=es_index, body={"sort": [ {"number": "asc" }], "from": index_from, "size": size, "query": {"match": {"status": "open"}}})
+    #print("Got %d Hits:" % res['hits']['total'])
+    tickets = [i['_source'] for i in res['hits']['hits'] ]
+    number_responses = res['hits']['total']
+    return number_responses, tickets
+
 
 if __name__ == "__main__":
     #print get_ticket_by_uuid('8449fae6-f97d-43b1-8c51-cb6c87990b11')
-    print get_ticket_by_number(247)
+    #print get_ticket_by_number(247)
+    print get_open_tickets()
--- snotrocket/test_pysnotrocket.py
+++ snotrocket/test_pysnotrocket.py
@@ -14,5 +14,8 @@ assert(get_ticket_by_number(247)['flags'] == 'WINTEL,HISS')
 uuid_tic = get_ticket_by_uuid('8449fae6-f97d-43b1-8c51-cb6c87990b11')
 
 assert(uuid_tic['from_line'] == 'nibz@cat.pdx.edu')
+num_tickets, tickets = get_open_tickets()
+assert(len(tickets) == 20)
+assert(num_tickets > 100)
 
 
