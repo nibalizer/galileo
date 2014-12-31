854d7bf0ddd3f434e3c0e218c272f88d1a088f0e
Spencer Krum <nibz@cat.pdx.edu>
2014-12-31 13:00:00 -0800

    version dat api

--- snotrocket/snotrocket_server.py
+++ snotrocket/snotrocket_server.py
@@ -11,25 +11,25 @@ def hello():
     return "Hello World!"
 
 
-@app.route("/tickets/number/<int:number>")
+@app.route("/v1/tickets/number/<int:number>")
 def get_ticket_number(number):
     resp = pysnotrocket.get_ticket_by_number(number)
     return jsonify(resp)
 
 
-@app.route("/tickets/uuid/<uuid>")
+@app.route("/v1/tickets/uuid/<uuid>")
 def get_ticket_uuid(uuid):
     resp = pysnotrocket.get_ticket_by_uuid(uuid)
     return jsonify(resp)
 
 
-@app.route("/tickets/open")
+@app.route("/v1/tickets/open")
 def find_open_tickets_bare():
     num_resp, tickets= pysnotrocket.get_open_tickets()
     return jsonify({"total": num_resp, "tickets": tickets})
 
 
-@app.route("/ticket/open/from=<int:index_from>/size=<int:size>")
+@app.route("/v1/ticket/open/from=<int:index_from>/size=<int:size>")
 def find_open_tickets(index_from, size):
     num_resp, tickets = pysnotrocket.get_open_tickets(index_from, size)
     return jsonify({"total": num_resp, "tickets": tickets})
