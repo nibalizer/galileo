691d574dc2b952c60a67b5f9f16d7eaf9ea6e94e
Spencer Krum <nibz@cat.pdx.edu>
2015-01-11 03:18:26 -0800

    New db, new test ticktes

--- snotrocket/test_pysnotrocket.py
+++ snotrocket/test_pysnotrocket.py
@@ -11,7 +11,13 @@ from pysnotrocket import *
 
 
 assert(get_ticket_by_number(247)['flags'] == 'WINTEL,HISS')
-uuid_tic = get_ticket_by_uuid('8449fae6-f97d-43b1-8c51-cb6c87990b11')
+# Note that the following test fails if you ever reset the ES store
+# From the snot files(db)
+# This is because the uuids are generated on the fly and only ever
+# written down in ES. This is okay because they are only short lived
+# pointers that boogerd hands out to refer to tickets until their
+# ticket number is assigned by snot
+uuid_tic = get_ticket_by_uuid('db9e0124-840f-47c0-92e4-6dfb8cdf8969')
 
 assert(uuid_tic['from_line'] == 'nibz@cat.pdx.edu')
 num_tickets, tickets = get_open_tickets()
