8d70aab1b0fae2c90188b03fc9a2296e93acfddd
Spencer Krum <nibz@cat.pdx.edu>
2015-01-11 03:01:43 -0800

    snotrocket now uses pysnot

--- snotrocket/snotrocket_populate.py
+++ snotrocket/snotrocket_populate.py
@@ -13,6 +13,7 @@ from elasticsearch import Elasticsearch
 
 # custom imports
 import snotparser.snotparser as sp
+import pysnot.pysnot as snot
 
 # by default we connect to localhost:9200
 es = Elasticsearch()
@@ -22,6 +23,17 @@ es = Elasticsearch()
 def import_ticket(ticket, es_index):
     t = int(ticket)
     parsed_data = sp.parseTicket(t, 'testsnot')
+    ticket_content = snot.get_ticket(t)
+    ticket_content_raw = snot.get_ticket_raw(t)
+    es.index(index=es_index,
+             doc_type="snot-ticket-content",
+             id=("{0}-content".format(ticket)),
+             body={"content": ticket_content})
+    es.index(index=es_index,
+             doc_type="snot-ticket-content-raw",
+             id=("{0}-content-raw".format(ticket)),
+             body={"content": ticket_content_raw})
+
     b = es.index(index=es_index, doc_type="snot-ticket", id=ticket, body=parsed_data)
 
 
@@ -36,9 +48,9 @@ def initial_import(active_dir, es_index):
     for ticket in tickets:
         t = int(ticket)
         import_ticket(t, es_index)
-        print ticket, parsed_data['status']
+        print ticket
 
 
 if __name__ == "__main__":
-    #initial_import('/u/snot/test/spool/active', 'snotrocket')
-    import_ticket(264, 'snotrocket')
+    initial_import('/u/snot/test/spool/active', 'snotrocket')
+    #import_ticket(264, 'snotrocket')
