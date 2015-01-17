4132a9c72cd17f1c799c78c7f9ac0d86ac2c763b
Spencer Krum <nibz@spencerkrum.com>
2015-01-17 00:20:59 -0800

    init secret signer

--- nostrild/nostrild.py
+++ nostrild/nostrild.py
@@ -14,10 +14,24 @@ def hello():
     return "nostrild: authentication for snot"
 
 
+@app.route("/auth", methods = ["POST"])
+def auth():
+    req = request.get_json(force=True)
+    print req
+    if req['user'] is None:
+        abort(400, "You must specify a user")
+    if req['password'] is None:
+        abort(400, "You must specify a password")
+
+
+    return 'yes'
+
+
 
 if __name__ == "__main__":
     with open('config.yaml') as f:
         conf = yaml.load(f.read())
     f.closed
+    s = TimestampSigner(conf['secret_key'])
 
     app.run(debug=True, port=conf['port'])
