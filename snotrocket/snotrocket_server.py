898dd34d436f279f225483e1185bbb3e169f866d
Spencer Krum <nibz@cat.pdx.edu>
2015-01-17 14:00:06 -0800

    use auth from nostrild

--- snotrocket/snotrocket_server.py
+++ snotrocket/snotrocket_server.py
@@ -1,4 +1,5 @@
 
+import yaml
 from flask import Flask, jsonify, request, abort
 from flask_cors import CORS
 from itsdangerous import TimestampSigner
@@ -9,20 +10,21 @@ import pysnotrocket
 app = Flask(__name__)
 cors = CORS(app)
 
-
-
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-
-@app.route("/v1/tickets/number/<int:number>")
-def get_ticket_number(number):
-    print request.headers
+def verify_auth():
     try:
         s.unsign(request.headers['X-SNOT-Auth-Key'], max_age=1500)
     except:
         abort(401)
+
+
+@app.route("/")
+def hello():
+    return "SnotRocket: search and read for SNOT"
+
+
+@app.route("/v1/tickets/number/<int:number>")
+def get_ticket_number(number):
+    verify_auth()
     resp = pysnotrocket.get_ticket_by_number(number)
     content = pysnotrocket.get_ticket_content(number)
     resp['content'] = content
@@ -31,6 +33,7 @@ def get_ticket_number(number):
 
 @app.route("/v1/tickets/uuid/<uuid>")
 def get_ticket_uuid(uuid):
+    verify_auth()
     resp = pysnotrocket.get_ticket_by_uuid(uuid)
     content = pysnotrocket.get_ticket_content(resp['number'])
     resp['content'] = content
@@ -39,16 +42,22 @@ def get_ticket_uuid(uuid):
 
 @app.route("/v1/tickets/open")
 def find_open_tickets_bare():
+    verify_auth()
     num_resp, tickets= pysnotrocket.get_open_tickets()
     return jsonify({"total": num_resp, "tickets": tickets})
 
 
 @app.route("/v1/ticket/open/from=<int:index_from>/size=<int:size>")
 def find_open_tickets(index_from, size):
+    verify_auth()
     num_resp, tickets = pysnotrocket.get_open_tickets(index_from, size)
     return jsonify({"total": num_resp, "tickets": tickets})
 
 
 if __name__ == "__main__":
-    s = TimestampSigner('secretstring')
-    app.run(debug=True,port=5001)
+    with open('config.yaml') as f:
+        conf = yaml.load(f.read())
+    f.closed
+    s = TimestampSigner(conf['secret_key'])
+
+    app.run(debug=conf['debug'],port=conf['port'])
